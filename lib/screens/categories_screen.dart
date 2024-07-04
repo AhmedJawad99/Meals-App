8002a3ced9cd6254448707af68fef75c6f01bf4f
Ahmed Jawad <jo367992@gmail.com>
2024-07-04 17:46:04 +0300

    animation of gridView

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -3,25 +3,62 @@ import 'package:mealapp/data/dummy_data.dart';
 import 'package:mealapp/models/meal.dart';
 import 'package:mealapp/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availabeMeals});
   final List<Meal> availabeMeals;
 
   @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this,
+        duration: const Duration(seconds: 1),
+        lowerBound: 0,
+        upperBound: 1);
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          mainAxisSpacing: 7,
-          crossAxisSpacing: 7,
-          childAspectRatio: 3 / 2),
-      children: [
-        for (final category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            availabeMeals: availabeMeals,
-          )
-      ],
-    );
+    return AnimatedBuilder(
+        animation: _controller,
+        child: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 7,
+              crossAxisSpacing: 7,
+              childAspectRatio: 3 / 2),
+          children: [
+            for (final category in availableCategories)
+              CategoryGridItem(
+                category: category,
+                availabeMeals: widget.availabeMeals,
+              )
+          ],
+        ),
+        builder: (ctx, child) => SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(0, 0.3),
+                end: const Offset(0, 0),
+              ).animate(
+                CurvedAnimation(
+                    parent: _controller, curve: Curves.linearToEaseOut),
+              ),
+              child: child,
+            ));
   }
 }
